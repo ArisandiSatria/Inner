5a8f94017d04a7cf9d94e456c0146e9fe36777e4
Arisandi <arisandisatria541@gmail.com>
2023-09-25 05:53:42 +0700

    Fixed overflowing content in splash screen, optimize layout in main screen and fixed navbar pushed up when keyboard pop up

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -7,57 +7,122 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 60, left: 30, right: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Penginapan yang Nyaman \nMembawa Kebahagiaan',
-                      style: blackTextStyle.copyWith(fontSize: 18)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Temukan penginapan dengan cepat, tepat, dan pastinya aman',
-                    style: greyTextStyle.copyWith(fontSize: 12),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    width: 210,
-                    height: 50,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return MainScreen();
-                          }));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
-                            backgroundColor: primaryColor),
-                        child: Text(
-                          'Jelajahi',
-                          style: TextStyle(fontSize: 18),
-                        )),
-                  ),
-                  // SizedBox(
-                  //   height: 23,
-                  // ),
-                ],
-              ),
-            ),
-            Spacer(),
-            Image.asset('assets/images/splash_img.png')
-          ],
-        ),
+        child: (orientation == Orientation.portrait) ? _columnLayout() : _rowLayout(),
       ),
+    );
+  }
+}
+
+class _rowLayout extends StatelessWidget {
+  const _rowLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Penginapan yang Nyaman \nMembawa Kebahagiaan',
+                  style: blackTextStyle.copyWith(fontSize: 18)),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Temukan penginapan dengan cepat, tepat, dan pastinya aman',
+                style: greyTextStyle.copyWith(fontSize: 12),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: 210,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return MainScreen();
+                      }));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
+                        backgroundColor: primaryColor),
+                    child: Text(
+                      'Jelajahi',
+                      style: TextStyle(fontSize: 18),
+                    )),
+              ),
+            ],
+          ),
+        ),
+        Spacer(),
+        Image.asset('assets/images/splash_img.png')
+      ],
+    );
+  }
+}
+
+class _columnLayout extends StatelessWidget {
+  const _columnLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Penginapan yang Nyaman \nMembawa Kebahagiaan',
+                  style: blackTextStyle.copyWith(fontSize: 18)),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Temukan penginapan dengan cepat, tepat, dan pastinya aman',
+                style: greyTextStyle.copyWith(fontSize: 12),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: 210,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return MainScreen();
+                      }));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
+                        backgroundColor: primaryColor),
+                    child: Text(
+                      'Jelajahi',
+                      style: TextStyle(fontSize: 18),
+                    )),
+              ),
+            ],
+          ),
+        ),
+        Spacer(),
+        Image.asset('assets/images/splash_img.png')
+      ],
     );
   }
 }
